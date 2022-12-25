60f1b1ce17e4b93f5c8b843d3c2f2c36201b2631
Aleksandra <olusiawis@gmail.com>
2022-12-25 21:42:03 +0100

    login,logout,signup

--- website/__init__.py
+++ website/__init__.py
@@ -4,4 +4,9 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "a1b2ppp"
 
+    from .views import views
+    from .auth import auth
+
+    app.register_blueprint(views, url_prefix='/')
+    app.register_blueprint(auth, url_prefix='/')
     return app

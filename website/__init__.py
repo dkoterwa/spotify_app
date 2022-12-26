adfaf12d54b1891aaf7b83d6d17405df1289496f
Dominik Koterwa <koterwadominik@icloud.com>
2022-12-26 14:33:44 +0100

    add file upload option

--- website/__init__.py
+++ website/__init__.py
@@ -1,7 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+import os
 
-def create_app():
-    app = Flask(__name__)
-    app.config["SECRET_KEY"] = "a1b2ppp"
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "a1b2ppp"
+app.config["UPLOAD_FOLDER"] = ""
 
-    return app
+@app.route("/")
+def welcome():
+    return "Welcome stranger"
+
+@app.route("/upload", methods=["POST", "GET"])
+def upload_file():
+    # Check if a file was uploaded
+    if "file" not in request.files:
+        return "No file was uploaded"
+
+    file = request.files["file"]
+
+    # Check if the file has a valid filename
+    if file.filename == "":
+        return "Please select a file"
+
+    # Save the file to the server
+    filename = secure_filename(file.filename)
+    file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+    return "File uploaded successfully"

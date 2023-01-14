13166c0258f0596d2faddfe3deaec3ffe08aeb45
Aleksandra <olusiawis@gmail.com>
2023-01-15 00:19:59 +0100

    webscrapping #found him

--- website/webscrapping.py
+++ website/webscrapping.py
@@ -1,19 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 import re
-
+import requests
+from unidecode import unidecode
 
 def get_main_wiki_image(artist):
-  person_url = []
-  urlpage =  'https://en.wikipedia.org/wiki/' + artist
-  page = requests.get(urlpage).text
-  soup = BeautifulSoup(page, 'html.parser')
-  for raw_img in soup.find_all('img'):
-   link = raw_img.get('src')
-   if re.search('wikipedia/.*/thumb/', link) and not re.search('.svg', link):
-     person_url = [artist, link]
-     break
-  try:
-    return person_url[1][2:]
-  except IndexError:
-    return "No image"
+    artist = unidecode(artist).replace(' ', '-')
+    person_url = []
+    urlpage =  'https://www.aaemusic.com/artist/' + artist
+    page = requests.get(urlpage).text
+    soup = BeautifulSoup(page, 'html.parser')
+    for raw_img in soup.find_all('img', {"class":"img-responsive artist-img lazyautosizes lazyload blur"}):
+        link = raw_img.get('src')
+        person_url = [artist, link]
+    # if re.search('wikipedia/.*/thumb/', link) and not re.search('.svg', link):
+    #   person_url = [artist, link]
+    #   break
+    try:
+        return person_url[1]
+    except IndexError:
+        return artist
